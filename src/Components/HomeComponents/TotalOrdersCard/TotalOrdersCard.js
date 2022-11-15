842df0cd3746e440140f425e88c4090435917a07
BhaweshPandey <pandeybhawesh12@gmail.com>
2022-11-15 17:27:04 +0530

    zawaha

--- src/Components/HomeComponents/TotalOrdersCard/TotalOrdersCard.js
+++ src/Components/HomeComponents/TotalOrdersCard/TotalOrdersCard.js
@@ -7,13 +7,13 @@ const TotalOrdersCard = () => {
     <div className='TotalOrdersCard'>
         <div className='TotalOrdersCard-top'>
             <div className='TotalOrdersCard-circle'>
-                <h1>168</h1>
-                <p>Total orders</p>
+                <h1 className='TotalOrdersCard-Circle-inear-h'>168</h1>
+                <p className='TotalOrdersCard-Circle-inear-p'>Total orders</p>
             </div>
         </div>
         <div className='TotalOrdersCard-mid'>
-            <HomeTopBox text={"Item for sale"} number={"168"} />
-            <HomeTopBox text={"Item sold"} number={"20"} />
+            <HomeTopBox text={"Item for sale"} number={"168"} styles={"TotalOrdersCard-mid-text"} />
+            <HomeTopBox text={"Item sold"} number={"20"} styles={"TotalOrdersCard-mid-text"} />
         </div>
         <div className='TotalOrdersCard-bottom'>
             <p>See inventory</p>
