44be7df9611e2a4389a1c6a1b79a336d4a43b07a
BhaweshPandey <pandeybhawesh12@gmail.com>
2022-12-21 16:32:46 +0530

    SeachingUpdate

--- src/Components/MyOrderComponents/ActiveOrder/ActiveOrder.js
+++ src/Components/MyOrderComponents/ActiveOrder/ActiveOrder.js
@@ -1,104 +1,203 @@
-import React ,{useState} from 'react'
+import React, { useState } from 'react'
 import './ActiveOrder.css'
 import AllImages from '../../../Assets/AllImages';
+// import data from '../../../Data/MyOrders.json'
+
+export const ActiveOrderData = [
+  {
+    code: 'XTRX2918882910',
+    name: 'Sophie Asveld',
+    Confirmed: 'Order placed',
+    Orderdate: 'nov 29th, 2022',
+    price: '$ 120.50',
+    EventDate: 'dec 25nd, 2022',
+    selectbtn: 'Proceed',
+    button_css: {
+      background: "white", border: '2px solid #EAECF3'
+    }
+  },
+  {
+    code: 'ABCD2918882910',
+    name: 'Sophie Asveld',
+    Confirmed: 'Confirmed',
+    Orderdate: 'nov 29th, 2022',
+    price: '$ 110.50',
+    EventDate: 'dec 25nd, 2022',
+    selectbtn: 'Proceed'
+  },
+  {
+    code: 'BDRCX2918882910',
+    name: 'Sophie Asveld',
+    Confirmed: 'On progress',
+    Orderdate: 'nov 29th, 2022',
+    price: '$ 126.50',
+    EventDate: 'dec 25nd, 2022',
+    selectbtn: 'Proceed'
+  },
+  {
+    code: 'CWER2918882910',
+    name: 'Sophie Asveld',
+    Confirmed: 'Order placed',
+    Orderdate: 'nov 29th, 2022',
+    price: '$ 130.50',
+    EventDate: 'dec 25nd, 2022',
+    selectbtn: 'Proceed',
+    button_css: {
+      background: "white", border: '2px solid #EAECF3'
+    }
+  },
+  {
+    code: 'DLKJH2918882910',
+    name: 'Sophie Asveld',
+    Confirmed: 'Order placed',
+    Orderdate: 'nov 29th, 2022',
+    price: '$ 119.50',
+    EventDate: 'dec 25nd, 2022',
+    selectbtn: 'Proceed',
+    button_css: {
+      background: "white", border: '2px solid #EAECF3'
+    }
+  },
+  {
+    code: 'FQWER2918882910',
+    name: 'Sophie Asveld',
+    Confirmed: 'Confirmed',
+    Orderdate: 'nov 29th, 2022',
+    price: '$ 120.50',
+    EventDate: 'dec 25nd, 2022',
+    selectbtn: 'Proceed'
+  },
+  {
+    code: 'MTYUI2918882910',
+    name: 'Sophie Asveld',
+    Confirmed: 'On progress',
+    Orderdate: 'nov 29th, 2022',
+    price: '$ 121.50',
+    EventDate: 'dec 25nd, 2022',
+    selectbtn: 'Proceed'
+  },
+  {
+    code: 'NZXCVG2918882910',
+    name: 'Sophie Asveld',
+    Confirmed: 'Order placed',
+    Orderdate: 'nov 29th, 2022',
+    price: '$ 124.50',
+    EventDate: 'dec 25nd, 2022',
+    selectbtn: 'Proceed',
+    button_css: {
+      background: "white", border: '2px solid #EAECF3'
+    }
+  },
+]
 
 
-const ActiveOrder = ({activebtn}) => {
-    const [confirmed, setConfirmed] = useState(false)
+const ActiveOrder = ({ activebtn, input }) => {
+  const [confirmed, setConfirmed] = useState(false)
 
-    const ActiveOrder = [
-        {
-          code: 'XTRX2918882910',
-          name: 'Sophie Asveld',
-          Confirmed: 'Order placed',
-          Orderdate: 'nov 29th, 2022',
-          price: '$ 120.50',
-          EventDate: 'dec 25nd, 2022',
-          selectbtn: 'Proceed',
-          button_css: {
-            background: "white", border:'2px solid #EAECF3' 
-        }
-        },
-        {
-          code: 'XTRX2918882910',
-          name: 'Sophie Asveld',
-          Confirmed: 'Confirmed',
-          Orderdate: 'nov 29th, 2022',
-          price: '$ 110.50',
-          EventDate: 'dec 25nd, 2022',
-          selectbtn: 'Proceed'
-        },
-        {
-          code: 'XTRX2918882910',
-          name: 'Sophie Asveld',
-          Confirmed: 'On progress',
-          Orderdate: 'nov 29th, 2022',
-          price: '$ 126.50',
-          EventDate: 'dec 25nd, 2022',
-          selectbtn: 'Proceed'
-        },
-        {
-          code: 'XTRX2918882910',
-          name: 'Sophie Asveld',
-          Confirmed: 'Order placed',
-          Orderdate: 'nov 29th, 2022',
-          price: '$ 130.50',
-          EventDate: 'dec 25nd, 2022',
-          selectbtn: 'Proceed',
-          button_css: {
-            background: "white", border:'2px solid #EAECF3' 
-        }},
-        {
-          code: 'XTRX2918882910',
-          name: 'Sophie Asveld',
-          Confirmed: 'Order placed',
-          Orderdate: 'nov 29th, 2022',
-          price: '$ 119.50',
-          EventDate: 'dec 25nd, 2022',
-          selectbtn: 'Proceed',
-          button_css: {
-            background: "white", border:'2px solid #EAECF3' 
-        }
-        },
-        {
-          code: 'XTRX2918882910',
-          name: 'Sophie Asveld',
-          Confirmed: 'Confirmed',
-          Orderdate: 'nov 29th, 2022',
-          price: '$ 120.50',
-          EventDate: 'dec 25nd, 2022',
-          selectbtn: 'Proceed'
-        },
-        {
-          code: 'XTRX2918882910',
-          name: 'Sophie Asveld',
-          Confirmed: 'On progress',
-          Orderdate: 'nov 29th, 2022',
-          price: '$ 121.50',
-          EventDate: 'dec 25nd, 2022',
-          selectbtn: 'Proceed'
-        },
-        {
-          code: 'XTRX2918882910',
-          name: 'Sophie Asveld',
-          Confirmed: 'Order placed',
-          Orderdate: 'nov 29th, 2022',
-          price: '$ 124.50',
-          EventDate: 'dec 25nd, 2022',
-          selectbtn: 'Proceed',
-          button_css: {
-            background: "white", border:'2px solid #EAECF3' 
-        }
-        },
-      ]
+  const filteredData = ActiveOrderData.filter((el) => {
+    //if no input the return the original
+    if (input === '') {
+      return el;
+    }
+    //return the item which contains the user input
+    else {
+      return el.code.toLowerCase(ActiveOrderData).includes(input)
+    }
+  })
 
-      if (!activebtn) {
-        return null ;
-      }
+  // const ActiveOrderData = [
+  //     {
+  //       code: 'XTRX2918882910',
+  //       name: 'Sophie Asveld',
+  //       Confirmed: 'Order placed',
+  //       Orderdate: 'nov 29th, 2022',
+  //       price: '$ 120.50',
+  //       EventDate: 'dec 25nd, 2022',
+  //       selectbtn: 'Proceed',
+  //       button_css: {
+  //         background: "white", border:'2px solid #EAECF3' 
+  //     }
+  //     },
+  //     {
+  //       code: 'XTRX2918882910',
+  //       name: 'Sophie Asveld',
+  //       Confirmed: 'Confirmed',
+  //       Orderdate: 'nov 29th, 2022',
+  //       price: '$ 110.50',
+  //       EventDate: 'dec 25nd, 2022',
+  //       selectbtn: 'Proceed'
+  //     },
+  //     {
+  //       code: 'XTRX2918882910',
+  //       name: 'Sophie Asveld',
+  //       Confirmed: 'On progress',
+  //       Orderdate: 'nov 29th, 2022',
+  //       price: '$ 126.50',
+  //       EventDate: 'dec 25nd, 2022',
+  //       selectbtn: 'Proceed'
+  //     },
+  //     {
+  //       code: 'XTRX2918882910',
+  //       name: 'Sophie Asveld',
+  //       Confirmed: 'Order placed',
+  //       Orderdate: 'nov 29th, 2022',
+  //       price: '$ 130.50',
+  //       EventDate: 'dec 25nd, 2022',
+  //       selectbtn: 'Proceed',
+  //       button_css: {
+  //         background: "white", border:'2px solid #EAECF3' 
+  //     }},
+  //     {
+  //       code: 'XTRX2918882910',
+  //       name: 'Sophie Asveld',
+  //       Confirmed: 'Order placed',
+  //       Orderdate: 'nov 29th, 2022',
+  //       price: '$ 119.50',
+  //       EventDate: 'dec 25nd, 2022',
+  //       selectbtn: 'Proceed',
+  //       button_css: {
+  //         background: "white", border:'2px solid #EAECF3' 
+  //     }
+  //     },
+  //     {
+  //       code: 'XTRX2918882910',
+  //       name: 'Sophie Asveld',
+  //       Confirmed: 'Confirmed',
+  //       Orderdate: 'nov 29th, 2022',
+  //       price: '$ 120.50',
+  //       EventDate: 'dec 25nd, 2022',
+  //       selectbtn: 'Proceed'
+  //     },
+  //     {
+  //       code: 'XTRX2918882910',
+  //       name: 'Sophie Asveld',
+  //       Confirmed: 'On progress',
+  //       Orderdate: 'nov 29th, 2022',
+  //       price: '$ 121.50',
+  //       EventDate: 'dec 25nd, 2022',
+  //       selectbtn: 'Proceed'
+  //     },
+  //     {
+  //       code: 'XTRX2918882910',
+  //       name: 'Sophie Asveld',
+  //       Confirmed: 'Order placed',
+  //       Orderdate: 'nov 29th, 2022',
+  //       price: '$ 124.50',
+  //       EventDate: 'dec 25nd, 2022',
+  //       selectbtn: 'Proceed',
+  //       button_css: {
+  //         background: "white", border:'2px solid #EAECF3' 
+  //     }
+  //     },
+  //   ]
+
+  if (!activebtn) {
+    return null;
+  }
   return (
     <div className='ActiveOrder'>
-        <div className='item-box-map'>
-        {ActiveOrder.map((item, id) => (
+      <div className='item-box-map'>
+        {filteredData.map((item, id) => (
           <div className='item-box'>
             <div className='item-box-top'>
               <div className='item-img'>
@@ -133,8 +232,8 @@ const ActiveOrder = ({activebtn}) => {
                   <p className='item-box-bottom-top'>Payment</p>
                   <h3 className='item-box-bottom-bottom'>Credit card</h3>
                 </div>
-                <button  onClick={() => setConfirmed(item.price)} className='item-box-bottom-btn2'>
-                {confirmed === item.price ? 'Confirmed' : item.selectbtn}
+                <button onClick={() => setConfirmed(item.price)} className='item-box-bottom-btn2'>
+                  {confirmed === item.price ? 'Confirmed' : item.selectbtn}
                 </button>
               </div>
             </div>
